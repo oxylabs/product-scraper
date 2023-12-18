1bfc615846f7e17facd66e7f58ff431e51b7c2cc
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 13:22:17 +0200

    Adding code examples/product_curl.sh

---
+++ code examples/product_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://bellroy.com/products/slim-sleeve-wallet?color=cocoa-java&material=leather&size=default"}'
